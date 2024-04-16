116545fcc90feb2943aea404dec3c2f384845f90
Samuel <samuelmahecha651@gmail.com>
2024-04-15 21:07:14 -0500

    test1

--- src/app/tasksync/room-login/room-login.component.spec.ts
+++ src/app/tasksync/room-login/room-login.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
 import { RoomLoginComponent } from './room-login.component';
 
 describe('RoomLoginComponent', () => {
@@ -8,9 +8,9 @@ describe('RoomLoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RoomLoginComponent]
-    })
-    .compileComponents();
+      declarations: [RoomLoginComponent],
+      imports: [FormsModule]
+    }).compileComponents();
     
     fixture = TestBed.createComponent(RoomLoginComponent);
     component = fixture.componentInstance;
@@ -20,31 +20,20 @@ describe('RoomLoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  it('should update username and roomName on user input', () => {
-    component.username = 'testUser';
-    component.roomName = 'testRoom';
-    fixture.detectChanges();
-    expect(component.username).toEqual('testUser');
-    expect(component.roomName).toEqual('testRoom');
-  });
+
   it('should call createRoom on button click', () => {
     spyOn(component, 'createRoom');
-    const button = fixture.nativeElement.querySelector('button#createRoomButton'); // Assuming you have a button with this id
+    const button = fixture.nativeElement.querySelector('button');
     button.click();
     expect(component.createRoom).toHaveBeenCalled();
   });
   
   it('should call joinRoom on button click', () => {
     spyOn(component, 'joinRoom');
-    const button = fixture.nativeElement.querySelector('button#joinRoomButton'); // Assuming you have a button with this id
-    button.click();
+    const buttons = fixture.nativeElement.querySelectorAll('button');
+    buttons[1].click();
     expect(component.joinRoom).toHaveBeenCalled();
   });
-  it('should call ngAfterViewInit', () => {
-    spyOn(component, 'ngAfterViewInit');
-    fixture.detectChanges();
-    expect(component.ngAfterViewInit).toHaveBeenCalled();
-  });
-      
 
+  // Se puede agregar más pruebas según sea necesario
 });
